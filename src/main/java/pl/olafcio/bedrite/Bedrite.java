8592e590da2ab56e2557d7f8a09c3dfb696d8af7
Olafcio <75330538+Olafcio1@users.noreply.github.com>
2026-03-28 14:27:58 +0100

    Initial commit

---
+++ src/main/java/pl/olafcio/bedrite/Bedrite.java
@@ -0,0 +1,10 @@
+package pl.olafcio.bedrite;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Bedrite implements ModInitializer {
+    @Override
+    public void onInitialize() {
+        //
+    }
+}
