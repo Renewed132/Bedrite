37ba7838f7095d8e3a12227b978b38393705e2e4
Olafcio <75330538+Olafcio1@users.noreply.github.com>
2026-03-29 22:00:22 +0200

    (26.2) Wolf aggressive when another wolf insta-killed

--- src/main/java/pl/olafcio/bedrite/mixin/MobEntityMixin.java
+++ src/main/java/pl/olafcio/bedrite/mixin/MobEntityMixin.java
@@ -1,7 +1,11 @@
 package pl.olafcio.bedrite.mixin;
 
+import net.minecraft.entity.ai.goal.GoalSelector;
+import net.minecraft.entity.damage.DamageSource;
 import net.minecraft.entity.mob.MobEntity;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
@@ -9,10 +13,19 @@ import pl.olafcio.bedrite.Feature;
 
 @Mixin(value = MobEntity.class, priority = 1500)
 public class MobEntityMixin {
+    @Shadow @Final protected GoalSelector attackGoals;
+
     @Inject(at = @At("HEAD"), method = "getVoidDamageDelay", cancellable = true)  // method from Renewed
     @Feature("Change void damage delay to 40ticks[/2s] instead of 1tick[/0.05s]")
     @SuppressWarnings("all")
     public void getVoidDamageDelay(CallbackInfoReturnable<Integer> cir) {
         cir.setReturnValue(-40);
+    }
+
+    @Inject(at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/mob/MobEntity;dropInventory(Lnet/minecraft/entity/damage/DamageSource;)V"), method = "damage")
+    @Feature("Wolf aggressive when another wolf insta-killed")
+    public void damage__killed(DamageSource source, int damage, CallbackInfoReturnable<Boolean> cir) {
+        for (int i = 0; i < 2; i++)
+            attackGoals.tick();
     }
 }
