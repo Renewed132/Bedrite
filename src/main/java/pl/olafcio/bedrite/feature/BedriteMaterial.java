d0398fcbdac6564855288e3325d75e702562bc60
Olafcio <75330538+Olafcio1@users.noreply.github.com>
2026-03-29 13:32:45 +0200

    Make enchanting tables hand-mineable

--- src/main/java/pl/olafcio/bedrite/feature/BedriteMaterial.java
+++ src/main/java/pl/olafcio/bedrite/feature/BedriteMaterial.java
@@ -4,7 +4,7 @@ import net.minecraft.block.material.Material;
 import net.minecraft.block.material.MaterialColor;
 
 public class BedriteMaterial extends Material {
-    public static final BedriteMaterial SMELTER = new BedriteMaterial(MaterialColor.STONE);
+    public static final BedriteMaterial STONE_NOTOOL = new BedriteMaterial(MaterialColor.STONE);
 
     public BedriteMaterial(MaterialColor materialColor) {
         super(materialColor);
--- src/main/java/pl/olafcio/bedrite/mixin/EnchantingTableBlockMixin.java
+++ src/main/java/pl/olafcio/bedrite/mixin/EnchantingTableBlockMixin.java
@@ -1,16 +1,16 @@
 package pl.olafcio.bedrite.mixin;
 
-import net.minecraft.block.FurnaceBlock;
+import net.minecraft.block.EnchantingTableBlock;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.ModifyArgs;
 import org.spongepowered.asm.mixin.injection.invoke.arg.Args;
 import pl.olafcio.bedrite.feature.BedriteMaterial;
 
-@Mixin(FurnaceBlock.class)
-public class FurnaceBlockMixin {
-    @ModifyArgs(at = @At(value = "INVOKE", target = "Lnet/minecraft/block/BlockWithEntity;<init>(ILnet/minecraft/block/material/Material;)V"), method = "<init>")
+@Mixin(EnchantingTableBlock.class)
+public class EnchantingTableBlockMixin {
+    @ModifyArgs(at = @At(value = "INVOKE", target = "Lnet/minecraft/block/BlockWithEntity;<init>(IILnet/minecraft/block/material/Material;)V"), method = "<init>")
     private static void init(Args args) {
-        args.set(1, BedriteMaterial.STONE_NOTOOL);
+        args.set(2, BedriteMaterial.STONE_NOTOOL);
     }
 }
