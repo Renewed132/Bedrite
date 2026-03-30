15bc393d3dfdc53720955cc8e69e02b1cc0c1d8b
Olafcio <75330538+Olafcio1@users.noreply.github.com>
2026-03-30 20:31:37 +0200

    (26.2) [GUI] New button textures, appropriate dimensions, improved text color, little changed positions

--- src/main/java/pl/olafcio/bedrite/mixin/TitleScreenMixin.java
+++ src/main/java/pl/olafcio/bedrite/mixin/TitleScreenMixin.java
@@ -10,23 +10,74 @@ import org.spongepowered.asm.mixin.injection.*;
 import org.spongepowered.asm.mixin.injection.invoke.arg.Args;
 import pl.olafcio.bedrite.mixininterface.ITextRenderer;
 
+import java.util.List;
+
 @Mixin(TitleScreen.class)
 public class TitleScreenMixin extends Screen {
     //---------//
     // BUTTONS //
     //---------//
 
-//    @ModifyConstant(constant = {
-//            @Constant(intValue = 100)
-//    }, method = {"init", "method_1724"})
-//    public int buttonWidth__screenHalf__minus(int constant) {
-//        return 98;
-//    }
-//
-//    @Redirect(at = @At(value = "NEW", target = "(IIILjava/lang/String;)Lnet/minecraft/client/gui/widget/ButtonWidget;"), method = {"init", "method_1724"})
-//    public ButtonWidget button__init(int x, int y, int color, String text) {
-//        return new ButtonWidget(x, y, color, 98, 20, text);
-//    }
+    @ModifyConstant(constant = {
+            @Constant(intValue = 100)
+    }, method = {"init", "method_1724"})
+    public int buttonWidth__screenHalf__minus(int constant) {
+        return 146/2;
+    }
+
+    @ModifyConstant(constant = {
+            @Constant(intValue = 24, ordinal = 1),
+            @Constant(intValue = 24, ordinal = 2)
+    }, method = "init")
+    public int button__heightNpad(int constant) {
+        return 30 + 2;
+    }
+
+    @Redirect(at = @At(value = "NEW", target = "(IIILjava/lang/String;)Lnet/minecraft/client/gui/widget/ButtonWidget;"), method = {"init", "method_1724"})
+    public ButtonWidget button__init(int id, int x, int y, String text) {
+        return new ButtonWidget(id, x, y, 146, 30, text);
+    }
+
+    @ModifyConstant(constant = {
+            @Constant(intValue = 48, ordinal = 0)
+    }, method = "init")
+    public int button__start(int constant) {
+        return 56;
+    }
+
+    @ModifyConstant(constant = {
+            @Constant(intValue = 48, ordinal = 1)
+    }, method = "init")
+    public int button__heightNpad2(int constant) {
+        return (30+2) * 2;
+    }
+
+    //--------//
+    // BUTTON //
+    //--------//
+
+    @Redirect(at = @At(value = "INVOKE", target = "Ljava/util/List;add(Ljava/lang/Object;)Z", ordinal = 3), method = "init")
+    public <E> boolean init__addButton__quit(List<E> instance, E e) {
+        return false;
+    }
+
+    @ModifyConstant(constant = {
+            @Constant(intValue = 98, ordinal = 0)
+    }, method = "init") public int init__newButton__options__width(int constant) {
+        return 146;
+    }
+    @ModifyConstant(constant = {
+            @Constant(intValue = 20, ordinal = 0)
+    }, method = "init") public int init__newButton__options__height(int constant) {
+        return 30;
+    }
+
+    @ModifyConstant(constant = {
+            @Constant(intValue = 72)
+    }, method = "init")
+    public int init__newButton__optLangQuit__y(int constant) {
+        return (30+2)*3 - 12;
+    }
 
     //-------//
     // TEXTS //
