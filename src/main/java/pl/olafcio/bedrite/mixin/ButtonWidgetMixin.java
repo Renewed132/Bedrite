08f73c211577bb4da74e3a26dc531988f3c14d8a
Olafcio <75330538+Olafcio1@users.noreply.github.com>
2026-03-30 20:31:37 +0200

    New button textures, appropriate dimensions, improved text color, little changed positions

--- src/main/java/pl/olafcio/bedrite/mixin/ButtonWidgetMixin.java
+++ src/main/java/pl/olafcio/bedrite/mixin/ButtonWidgetMixin.java
@@ -1,6 +1,5 @@
 package pl.olafcio.bedrite.mixin;
 
-import net.minecraft.client.font.TextRenderer;
 import net.minecraft.client.gui.widget.ButtonWidget;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.*;
@@ -15,8 +14,29 @@ public class ButtonWidgetMixin {
         return 0x4b4b4b;
     }
 
+    @ModifyConstant(constant = {
+            @Constant(intValue = 0xffffa0)
+    }, method = "method_891")
+    public int render__textColor__hover(int constant) {
+        return 0xefefef;
+    }
+
     @Redirect(at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/widget/ButtonWidget;drawCenteredString(Lnet/minecraft/client/font/TextRenderer;Ljava/lang/String;III)V"), method = "method_891")
     public void render__drawCenteredString(ButtonWidget instance, @Coerce ITextRenderer textRenderer, String text, int centerX, int y, int color) {
         textRenderer.drawCenteredStringWithoutShadow(text, centerX, y, color, false);
+    }
+
+    @ModifyConstant(constant = {
+            @Constant(intValue = 46)
+    }, method = "method_891")
+    public int texture_v(int constant) {
+        return 146;
+    }
+
+    @ModifyConstant(constant = {
+            @Constant(intValue = 20)
+    }, method = "method_891")
+    public int texture_v__state_mul(int constant) {
+        return 30;
     }
 }
